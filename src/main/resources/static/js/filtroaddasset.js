0a125b46509e54a924a3185c44d398721596261b
Vitor Augusto Genovez <59649115+vgenovez@users.noreply.github.com>
2020-02-08 21:31:23 -0300

    Botao para copiar o texto do addasset
    Desenvolvendo o botão para copiar o texto

--- src/main/resources/static/js/filtroaddasset.js
+++ src/main/resources/static/js/filtroaddasset.js
@@ -11,4 +11,16 @@ if (recebeStAtivo == "Ativo"){
     document.getElementById("formativo").style.display = "none";
     document.getElementById("formestoque").style.display = "none";
 }
+
+// Funçao para copiar
+
+$('#btcopiar').click(function(){
+$('#formativo').select();
+try {
+        var ok = document.execCommand('copy');
+        if (ok) { alert('Texto copiado para a área de transferência'); }
+    } catch (e) {
+    alert(e)
+}
+});
     
